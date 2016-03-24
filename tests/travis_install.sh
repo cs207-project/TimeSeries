7ed684bd5df324b673d165452c49cf00c9870b8f
ytang14 <tang01@g.harvard.edu>
2016-03-24 01:07:10 -0400

    asdf

--- tests/travis_install.sh
+++ tests/travis_install.sh
@@ -29,6 +29,7 @@ if [[ "$DISTRIB" == "conda" ]]; then
     source activate testenv
 
 elif [[ "$DISTRIB" == "ubuntu" ]]; then
+    echo hi
     # Use standard ubuntu packages in their default version
 fi
 
