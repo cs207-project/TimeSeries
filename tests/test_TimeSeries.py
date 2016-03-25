04c8fc4d67f53f92d6169b5d7516c906fb7f722a
mikezcc <zcc_zudiac@hotmail.com>
2016-03-24 22:53:35 -0400

    Code refactor

--- tests/test_TimeSeries.py
+++ tests/test_TimeSeries.py
@@ -4,17 +4,20 @@ import timeseries.TimeSeries as TS
 from timeseries.lazy import *
 import collections
 
-testSeries = TS.TimeSeries(range(0,4),range(1,5))
-testSeries2 = TS.TimeSeries(range(0,4),[10,20,30,40,50])
+# Test data
+ts1 = TS.TimeSeries(range(0,4),range(1,5))
+ts2 = TS.TimeSeries(range(0,4),[10,20,30,40,50])
+ts3 = TS.TimeSeries([0,5,10], [1,2,3])
+ts4 = TS.TimeSeries([2.5,7.5], [100, -100])
 
 def test_len():
-    assert len(testSeries) == 4
+    assert len(ts1) == 4
 
 def test_contains():
-    assert (2 in testSeries) == True
+    assert (2 in ts1) == True
 
 def test_getitem():
-    assert testSeries[0] == 1
+    assert ts1[0] == 1
 
 def test_setitem():
     tmpTestSeries = TS.TimeSeries(range(0,4),range(1,5))
@@ -23,13 +26,13 @@ def test_setitem():
 
 def test_equal():
     tmpTestSeries = TS.TimeSeries(range(0,4),range(1,5))
-    assert tmpTestSeries == testSeries
+    assert tmpTestSeries == ts1
 
 def test_values():
-    assert np.array_equal(testSeries.values(), range(1,5)) == True
+    assert np.array_equal(ts1.values(), range(1,5)) == True
 
 def test_time():
-    assert np.array_equal(testSeries.times(), range(0,4)) == True
+    assert np.array_equal(ts1.times(), range(0,4)) == True
 
 def test_mean_empty():
     with raises(ValueError):
@@ -40,47 +43,43 @@ def test_median_empty():
          TS.TimeSeries([],[]).median()
 
 def test_mean():
-    assert testSeries.mean() == 2.5
+    assert ts1.mean() == 2.5
 
 def test_median():
-    assert testSeries.median() == 2.5
+    assert ts1.median() == 2.5
 
 def test_itertimes():
-    ti=testSeries.itertimes()
+    ti=ts1.itertimes()
     assert next(ti)==0
 
 def test_itervalue():
-    vi=testSeries.itervalues()
+    vi=ts1.itervalues()
     assert next(vi)==1
 
 def test_iteritems():
-    iti=testSeries.iteritems()
+    iti=ts1.iteritems()
     assert next(iti)==(0,1)
 
 def test_interpolation():
-    a = TS.TimeSeries([0,5,10], [1,2,3])
-    b = TS.TimeSeries([2.5,7.5], [100, -100])
     # Simple cases
     c = TS.TimeSeries([1.],[1.2])
     d = a.interpolate([1.])
     assert c == d
-    assert a.interpolate(b.times()) == TS.TimeSeries([2.5,7.5], [1.5, 2.5])
+    assert ts3.interpolate(ts4.times()) == TS.TimeSeries([2.5,7.5], [1.5, 2.5])
     # Boundary conditions
-    assert a.interpolate([-100,100]) == TS.TimeSeries([-100,100],[1,3])
+    assert ts3.interpolate([-100,100]) == TS.TimeSeries([-100,100],[1,3])
     
 def test_add():
-    a = TS.TimeSeries([0,5,10], [1,2,3])
-    b = TS.TimeSeries([0,5,10], [10,20,30])
     c = 100
     d = TS.TimeSeries([0,1,2], [1,2,3])
-    assert a+b == TS.TimeSeries([0,5,10],[11,22,33])
-    assert a+c == TS.TimeSeries([0,5,10],[101,102,103])
+    assert ts1+ts2 == TS.TimeSeries(range(0,4),[11,22,33,44,55])
+    assert ts1+c == TS.TimeSeries(range(0,4),[101,102,103,104,105])
     with raises(ValueError):
-        a+d
-    assert a+b == b+a
-    assert c+a == a+c
+        ts1+d
+    assert ts1+ts2 == ts2+ts1
+    assert c+ts1 == ts1+c
     with raises(ValueError):
-        d+a
+        d+ts1
 
 def test_sub():
     a = TS.TimeSeries([0,5,10], [1,2,3])
