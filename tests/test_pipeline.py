faa80116b99ca0ce85e94f2f8a66d6f71ba17255
mikezcc <zcc_zudiac@hotmail.com>
2016-03-25 19:11:33 -0400

    refactored test file

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -2,27 +2,7 @@ from pype.lexer import lexer
 from pype.parser import parser
 from pype.semantic_analysis import CheckSingleAssignment, PrettyPrint
 from pype.translate import SymbolTableVisitor
-
-class Pipeline(object):
-  def __init__(self, source):
-    with open(source) as f:
-      self.compile(f)
-
-  def compile(self, file):
-    input = file.read()
-    # Lexing, parsing, AST construction
-    ast = parser.parse(input, lexer=lexer)
-    # Pretty print in ast class
-    ast.pprint()
-    # Pretty print using node visitor
-    ast.walk( PrettyPrint() )
-    # Semantic analysis
-    ast.walk( CheckSingleAssignment() )
-    # Translation
-    syms = ast.walk( SymbolTableVisitor())
-    # Pretty print symbol table
-    syms.pprint()
-    return syms
+from pype.Pipeline import Pipeline
 
 def test_example0():
     t=Pipeline('./samples/example0.ppl')
