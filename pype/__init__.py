491fa4619dfbd6bae735a54594cbf52138230bcd
mikezcc <zcc_zudiac@hotmail.com>
2016-03-26 14:32:00 -0400

    Move sample folder inside test

--- pype/__init__.py
+++ pype/__init__.py
@@ -1,12 +1,6 @@
 import pkg_resources
-from pype.semantic_analysis import *
-from pype.lexer import *
-from pype.ast import *
-from pype.parser import *
-from pype.pipeline import *
-from pype.translate import *
-from pype.symtab import *
-from pype.lib_import import *
+from .pipeline import Pipeline
+from .lib_import import component
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
 except:
--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,7 +1,7 @@
 from pype.pipeline import Pipeline
 
 def test_example0():
-    t=Pipeline('./samples/example0.ppl')
+    t=Pipeline('tests/samples/example0.ppl')
 
 def test_example1():
-    t=Pipeline('./samples/example1.ppl')
+    t=Pipeline('tests/samples/example1.ppl')
