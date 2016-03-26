414fd1f342203c48ab0b7ff082886111662d005f
mikezcc <zcc_zudiac@hotmail.com>
2016-03-26 18:04:16 -0400

    rename test folder

---
+++ test/test_pipeline.py
@@ -0,0 +1,7 @@
+from pype.pipeline import Pipeline
+
+def test_example0():
+    t=Pipeline('test/samples/example0.ppl')
+
+def test_example1():
+    t=Pipeline('test/samples/example1.ppl')
--- tests/test_pipeline.py
+++
@@ -1,7 +0,0 @@
-from pype.pipeline import Pipeline
-
-def test_example0():
-    t=Pipeline('tests/samples/example0.ppl')
-
-def test_example1():
-    t=Pipeline('tests/samples/example1.ppl')
