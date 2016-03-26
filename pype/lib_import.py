79cec1d107d9d8c1705387d67b326fc8dad53dba
mikezcc <zcc_zudiac@hotmail.com>
2016-03-26 19:28:20 -0400

    Added comments for lib_import

--- pype/lib_import.py
+++ pype/lib_import.py
@@ -45,20 +45,27 @@ class LibraryImporter(object):
     self.mod = importlib.import_module(modname)
 
   def add_symbols(self, symtab):
+    """
+      add a symbol to symtab
+      it should be named name
+      its type should be a libraryfunction SymbolType
+      its ref should be the object itself (obj)
+      
+      check if method was decorated like before
+      add a symbol like before, but with type librarymethod
+      (the ref should be the method, not obj)
+
+      Parameters
+      ----------
+      func: function
+        Function needs to be checked.
+    """
     assert self.mod is not None, 'No module specified or loaded'
     for (name,obj) in inspect.getmembers(self.mod):
       if inspect.isroutine(obj) and is_component(obj):
-        # TODO: add a symbol to symtab
-        #       it should be named name
-        #       its type should be a libraryfunction SymbolType
-        #       its ref should be the object itself (obj)
         symtab.addsym( Symbol(name, SymbolType.libraryfunction, obj) )
       elif inspect.isclass(obj):
         for (methodname,method) in inspect.getmembers(obj):
-           # TODO:
-           #   check if method was decorated like before
-           #   add a symbol like before, but with type librarymethod
-           #   (the ref should be the method, not obj)
           if inspect.isroutine(method) and is_component(method):
             symtab.addsym( Symbol(methodname, SymbolType.librarymethod, method) )
     return symtab
--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -7,16 +7,14 @@ example0_token='test/samples/example0.tokens'
 example1_ppl='test/samples/example1.ppl'
 example1_token='test/samples/example1.tokens'
 
-def test_lexer_error():
-    lexer.input(open(example_error_ppl).read())
-    for token in lexer:
-        print (token)
-
 def test_lexer():
     lexer.input(open(example1_ppl).read())
     output=open(example1_token)
     for token, line in zip(lexer, output):
         assert str(token) == line.strip()
+    lexer.input(open(example_error_ppl).read())
+    for token in lexer:
+        print (token)
 
 def test_example0():
     t=Pipeline(example0_ppl)
