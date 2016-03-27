124ad16a96edae126e77a4b87c4dfd1ddc85b443
JEEWON HWANG <jeewonhwang@g.harvard.edu>
2016-03-26 23:34:50 -0400

    add docstring and test cases for Lazy part

---
+++ test/test_lazy.py
@@ -0,0 +1,30 @@
+from pytest import raises
+import numpy as np
+from timeseries.lazy import *
+
+@lazy
+def lazy_add(a,b):
+    return a+b
+
+@lazy
+def lazy_mul(a,b):
+    return a*b
+
+@lazy
+def lazy_mul_three(a,b, c):
+    return a*b*c
+
+@lazy
+def check_length(a,b):
+    return len(a)==len(b)
+
+
+def test_eval():
+    thunk = lazy_mul( lazy_mul_three(1,lazy_add(1,1),lazy_add(1,2)), 4)
+    assert thunk.eval() == 24
+
+    thunk = check_length(TimeSeries(range(0,4),range(1,5)).lazy, TimeSeries(range(1,5),range(2,6)))
+    assert thunk.eval()==True
+
+def test_lazy():
+    assert isinstance( lazy_add(1,2), LazyOperation )==True
--- timeseries/lazy.py
+++ timeseries/lazy.py
@@ -1,17 +1,52 @@
 class LazyOperation():
+    '''
+    '''
     def __init__(self, function, *args, **kwargs):
+        '''
+        This initializes a LazyOperation instance with
+        inputs of a function, and other arguments.
+
+        Parameters
+        ----------
+        function : function
+            function that this LazyOperation() instance actually operates as
+
+        Examples
+        --------
+        >>> @lazy
+            def lazy_add(a,b):
+                return a+b
+        >>> isinstance( lazy_add(1,2), LazyOperation )
+        True
+        '''
         self.function = function
         self.args = args
         self.kwargs = kwargs
-        
+
     def eval(self):
+        '''
+        Evaluates the actual value of LazyOperation() instances.
+
+        Returns
+        -------
+        int
+            value
+
+        Examples
+        --------
+        >>> @lazy
+            def lazy_add(a,b):
+                return a+b
+        >>> lazy_add(2,4).eval()
+        6
+        '''
         temp_list = []
         for arg in self.args[0]:
             if isinstance(arg , LazyOperation):
                 temp_list.append(arg .eval())
             else:
                 temp_list.append(arg)
-            
+
         return self.function(*temp_list)
 
 def lazy(old_function):
