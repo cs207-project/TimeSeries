65acb8c25d3a387e24f54af0791933cb41e694e4
JEEWON HWANG <jeewonhwang@g.harvard.edu>
2016-05-01 13:55:41 -0400

    get ready for aiohttp

---
+++ web/web_application.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiohttp import web
+from tsdb import TSDBClient
+# from tsdb import TSDBStatus
+import timeseries as ts
+import json
+
+async def hello(request):
+    print(request)
+    return web.Response(body=b"Hello, world")
+
+app = web.Application()
+app.router.add_route('GET', '/', hello)
+# app.router.add_route('GET', '/tsdb/test', handler.test_upserts_handler)
+# app.router.add_route('GET','/tsbd/select',handler.select_handler)
+web.run_app(app)
