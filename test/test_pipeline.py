25b5b4255acda8f3b5e9ac154fd59fc8fa091376
Elena-Zhao <zhaoqing513@126.com>
2016-04-30 22:34:44 -0400

    mini fix

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -14,8 +14,8 @@ input1 = """(import timeseries)
 }"""
 
 
-example_error_ppl='./samples/example_error.ppl'
-example0_ppl='../test/samples/example0.ppl'
+example_error_ppl='test/samples/example_error.ppl'
+example0_ppl='test/samples/example0.ppl'
 example0_token='test/samples/example0.tokens'
 example1_ppl='test/samples/example1.ppl'
 example1_token='test/samples/example1.tokens'
