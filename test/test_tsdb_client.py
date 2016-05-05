adbc92005bb7d058368099ae3563e75a1910a6b5
Elena-Zhao <zhaoqing513@126.com>
2016-05-05 15:31:35 -0400

    add sleep time for tsdb client

--- test/test_tsdb_client.py
+++ test/test_tsdb_client.py
@@ -4,6 +4,7 @@ import numpy as np
 import subprocess
 import asynctest
 from scipy.stats import norm
+import time
 
 def tsmaker(m, s, j):
     "returns metadata and a time series in the shape of a jittered normal"
@@ -48,11 +49,13 @@ class Test_TSDB_Client(asynctest.TestCase):
             self.client.add_trigger('corr', 'insert_ts', ["d_vp-{}".format(i)], self.tsdict[self.vpkeys[i]])
             # change the metadata for the vantage points to have meta['vp']=True
             self.metadict[self.vpkeys[i]]['vp']=True
+        time.sleep(2)
 
     def tearDown(self):
         # Shuts down the server
         self.server_proc.terminate()
         self.server_log_file.close()
+        time.sleep(1)
 
     def test_upsert(self):
         print('Test upsert')
