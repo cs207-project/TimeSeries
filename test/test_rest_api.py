2aaf23dc304377cca32c473e7a5ce31d252089d4
JEEWON HWANG <jeewonhwang@g.harvard.edu>
2016-05-06 23:02:40 -0400

    vpdist key test

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -23,6 +23,17 @@ def tsmaker(m, s, j):
     v = norm.pdf(t, m, s) + j*np.random.randn(100)
     return meta, ts.TimeSeries(t, v)
 
+def select_to_str(md=None, fields=None, additional=None):
+    return_str= '{'
+    if md is not None:
+        return_str+='"md":'+str(md)
+    if fields is not None:
+        return_str+='"fields":'+str(fields)
+    if additional is not None:
+        return_str+='"additional":'+str(additional)
+    return_str+='}'
+    return return_str
+
 def insert_ts_to_json(primary_key, ts):
     return json.dumps({'primary_key':primary_key, 'ts':ts.to_json()})
 
@@ -36,6 +47,8 @@ def add_trigger_to_json(proc, onwhat, target, arg):
 
 def make_remove_trigger(proc, onwhat):
     return json.dumps({'proc':proc,'onwhat':onwhat})
+
+
 
 class RestApiTest(unittest.TestCase):
     def setUp(self):
@@ -119,17 +132,20 @@ class RestApiTest(unittest.TestCase):
         params = '{"fields":["order"]}'
         r = requests.get(self.web_url+'/select?query='+params)
         self.assertEqual(r.status_code, 200)
-        # #we first create a query time series.
-        # _, query = tsmaker(0.5, 0.2, 0.1)
-        #
-        # # Step 1: in the vpdist key, get  distances from query to vantage points
-        # # this is an augmented select
-        # vpdist = {}
-        # payload = {'proc':'corr','target':'d','arg':query.to_json()}
+
+        #we first create a query time series.
+        _, query = tsmaker(0.5, 0.2, 0.1)
+
+        # Step 1: in the vpdist key, get  distances from query to vantage points
+        # this is an augmented select
+        vpdist = {}
+        print(type(query.to_json()))
+        payload = {'proc':'corr', 'target':'d', 'arg':query.to_json()}
         # for v in vpkeys:
         #     payload['where'] = {'pk': v}
-        #     r = requests.get(self.server_url+'/augselect',params={'query':json.dumps(payload)})
+        #     r = requests.get(self.web_url+'/augmented_select', params={'query':json.dumps(payload)})
         #     results = json.loads(r.content.decode('utf-8'))
+        #     print(results)
         #     vpdist[v] = results[v]['d']
         #
         # closest_vpk = min(vpkeys,key=lambda v:vpdist[v])
@@ -149,114 +165,3 @@ class RestApiTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-# class RestApiTest(unittest.TestCase):
-#     def setUp(self):
-#         '''Method or coroutine called to prepare the test fixture.'''
-#         self.server_log_file = open('.test_rest_api_server_log', 'w')
-#         self.server_proc = subprocess.Popen(['python', 'go_server.py'], stdout=self.server_log_file, stderr=subprocess.STDOUT)
-#         print("popen go server")
-#         time.sleep(10)
-#
-#         self.web_url = 'http://localhost:8080/tsdb'
-#         self.web_log_file = open('.test_rest_api_web_log', 'w')
-#         self.web_proc = subprocess.Popen(['python', 'go_web.py'], stdout=self.web_log_file, stderr=subprocess.STDOUT)
-#         print("P oepn go web")
-#         time.sleep(10)
-#
-#     # def tearDown(self):
-#     #     '''Method called immediately after the test method has been called and the result recorded.'''
-#     #     self.server_proc.terminate()
-#     #     self.server_log_file.close()
-#     #
-#     #     self.web_proc.terminate()
-#     #     self.web_log_file.close()
-#
-#     def test_addTrigger(self):
-#         data = self.add_trigger_to_json('junk', 'insert_ts', None, 'db:one:ts')
-#         response = requests.post(self.web_url+'/add_trigger', data)
-#         print("did i come to trigger?")
-#         self.assertEqual(response.status_code, 200)
-#
-#     def add_trigger_to_json(self, proc, onwhat, target, arg):
-#         # arg is mostly ts
-#         if hasattr(arg, 'to_json'):
-#             arg = arg.to_json()
-#         return json.dumps({'proc':proc, 'onwhat':onwhat, 'target':target, 'arg':arg})
-#
-#     def test_main(self):
-#         pass
-#         # r = requests.get(self.web_url+'/select?query={}')
-#         # response = requests.post(self.web_url+'/add_trigger', data)
-#         # self.assertEqual(response.status_code, 200)
-#         #
-#         # r = requests.post(self.web_url+'/add/trigger',make_add_trigger(
-#         #                         'stats', 'insert_ts', ['mean', 'std'], None))
-#         # self.assertEqual(r.status_code, 200)
-#         # np.random.seed(12345)
-#         # N_ts = 50
-#         # N_vp = NUMVPS
-#         #
-#         # #Set up 50 time series
-#         # mus = np.random.uniform(low=0.0, high=1.0, size=N_ts)
-#         # sigs = np.random.uniform(low=0.05, high=0.4, size=N_ts)
-#         # jits = np.random.uniform(low=0.05, high=0.2, size=N_ts)
-#         #
-#         # # dictionaries for time series and their metadata
-#         # tsdict={}
-#         # metadict={}
-#         # for i, m, s, j in zip(range(N_ts), mus, sigs, jits):
-#         #     meta, tsrs = tsmaker(m, s, j)
-#         #     # the primary key format is ts-1, ts-2, etc
-#         #     pk = "ts-{}".format(i)
-#         #     tsdict[pk] = tsrs
-#         #     meta['vp'] = False # augment metadata with a boolean asking if this is a  VP.
-#         #     metadict[pk] = meta
-#         #
-#         # # choose 5 distinct vantage point time series
-#         # vpkeys = ["ts-{}".format(i) for i in np.random.choice(range(N_ts), size=N_vp, replace=False)]
-#         # for i in range(N_vp):
-#         #     # add 5 triggers to upsert distances to these vantage points
-#         #     requests.post(self.web_url+'/add/trigger',
-#         #                   make_add_trigger('corr', 'insert_ts', ["d_vp-{}".format(i)], tsdict[vpkeys[i]]))
-#         #     # change the metadata for the vantage points to have meta['vp']=True
-#         #     metadict[vpkeys[i]]['vp']=True
-#         # # Having set up the triggers, now inser the time series, and upsert the metadata
-#         # for k in tsdict:
-#         #     requests.post(self.web_url+'/add/ts',
-#         #                   make_insert_ts(k, tsdict[k]))
-#         #     requests.post(self.web_url+'/add/metadata',
-#         #                   make_upsert_meta(k, metadict[k]))
-#         #
-#         # #we first create a query time series.
-#         # _, query = tsmaker(0.5, 0.2, 0.1)
-#         #
-#         # # Step 1: in the vpdist key, get  distances from query to vantage points
-#         # # this is an augmented select
-#         # vpdist = {}
-#         # payload = {'proc':'corr','target':'d','arg':query.to_json()}
-#         # for v in vpkeys:
-#         #     payload['where'] = {'pk': v}
-#         #     r = requests.get(self.web_url+'/augselect',params={'query':json.dumps(payload)})
-#         #     results = json.loads(r.content.decode('utf-8'))
-#         #     vpdist[v] = results[v]['d']
-#         #
-#         # closest_vpk = min(vpkeys,key=lambda v:vpdist[v])
-#         #
-#         # # Step 2: find all time series within 2*d(query, nearest_vp_to_query)
-#         # #this is an augmented select to the same proc in correlation
-#         # payload = {'proc':'corr','target':'d','arg':query.to_json()}
-#         # payload['where'] = {'d_'+closest_vpk: {'<=': 2*vpdist[closest_vpk]}}
-#         # r = requests.get(self.web_url+'/augselect',params={'query':json.dumps(payload)})
-#         # results = json.loads(r.content.decode('utf-8'))
-#         #
-#         # #2b: find the smallest distance amongst this ( or k smallest)
-#         # #you can do this in local code
-#         # nearestwanted = min(results.keys(),key=lambda p: results[p]['d'])
-#         #
-#         # self.assertEqual(nearestwanted,'ts-35')
-#
-# if __name__ == '__main__':
-#     unittest.main()
